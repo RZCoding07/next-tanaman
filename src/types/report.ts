c76ff928be81a1925f570dd18384eb1f1d969ab4
Hafidz Alfatih <hafidzalfatih04@gmail.com>
2024-07-23 14:33:44 +0700

    refactor: Update ReportType interface with additional properties

--- src/types/report.ts
+++ src/types/report.ts
@@ -1,7 +1,18 @@
 export interface ReportType {
-    id: number;
-    bulan : string;
-    tahun : number;
-    createdAt : string;
-  }
-  
+  id: number;
+  kondisi: string;
+  status_umur: string;
+  kebun: string;
+  kkl_kebun: string;
+  afd: string;
+  tahun_tanam: number;
+  no_blok: string;
+  luas: number;
+  jlh_pokok: number;
+  pkk_ha: number;
+  r: number;
+  warna: string;
+  bulan : string;
+  tahun : number;
+  createdAt : string;
+}
