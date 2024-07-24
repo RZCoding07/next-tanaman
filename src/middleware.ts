8de71dc898f14f54760e515d82a078eb26bd3b6a
Hafidz Alfatih <hafidzalfatih04@gmail.com>
2024-07-24 15:09:19 +0700

    chore: Update npm dependencies and remove unused code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,10 @@
-export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-};
+// export const config = {
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
+
+import { NextResponse } from "next/server";
+
+export function middleware(req: Request) {
+  // Add a header to the response
+  return NextResponse.next();
+}
